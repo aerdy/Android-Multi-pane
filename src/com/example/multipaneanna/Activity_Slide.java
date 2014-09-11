306fae13a8f08b02bacd22affe2b26749f2da2ba
aerdy <aerdy4@gmail.com>
2014-09-11 11:25:40 +0700

    project

---
+++ src/com/example/multipaneanna/Activity_Slide.java
@@ -0,0 +1,17 @@
+package com.example.multipaneanna;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Activity_Slide extends Fragment{
+@Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	View tampil = inflater.inflate(R.layout.activity_slide, container,false);
+	return tampil;
+}
+}
